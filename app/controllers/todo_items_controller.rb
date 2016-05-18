d760e44256af544a9202b5b7a29cc9b99be41243
Lmilne <lmilne360@gmail.com>
2016-05-18 00:24:15 -0400

    Edited controller

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -9,7 +9,7 @@ class TodoItemsController < ApplicationController
 
   def create
   	@todo_list = TodoList.find(params[:todo_list_id])
-  	@todo_item = @todo_list.todo_items.new(todo_items_params)
+  	@todo_item = @todo_list.todo_items.new(todo_item_params)
 	  if @todo_item.save
 	  	flash[:success] = "Added todo list item."
 	  	redirect_to todo_list_todo_items_path
@@ -44,7 +44,7 @@ class TodoItemsController < ApplicationController
   private
 
   def todo_item_params
-  	params[:todo_item].permit(:content)
+  	params.require(:todo_item).permit(:content)
   	
   end
 end
